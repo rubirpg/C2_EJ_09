c79c7e0b63d9afda13e9e16f4021845a057c57ec
Rubirpg <rubi.rpg@gmail.com>
2022-07-27 13:00:05 +0200

    Agregar archivos de proyecto.

---
+++ C2_EJ_09/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2_Ej_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el valor para A: ");
+            int numa = Convert.ToInt32(Console.ReadLine()); //leemos y asignamos a numa el valor de A
+            Console.WriteLine("Introduzca el valor para B: ");
+            int numb = Convert.ToInt32(Console.ReadLine()); //leemos y asignamos a numb el valor de B
+
+            Console.WriteLine();
+            if (numa > numb) //Si A es mayor que B
+            {
+                Console.WriteLine("El valor de A es mayor que B.");
+            }
+            else
+            {
+                if (numb > numa) //Si B es mayor que A
+                {
+                    Console.WriteLine("El valor de B es mayor que A.");
+                }
+                else //Si A no es mayor que B y B no es mayor que A solo queda que sean iguales
+                    Console.WriteLine("El valor de A es igual que el de B.");
+            }
+        }
+    }
+}
